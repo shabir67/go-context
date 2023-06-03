eb2216d1f4145e0f667eba2242cd4ce331397c8c
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-03 16:52:29 +0700

    Implemented context "Background" & "Todo whereas both consists of empty context

---
+++ context_test.go
@@ -0,0 +1,16 @@
+package gocontext
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+
+}
