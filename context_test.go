00c08969fd732e58a5d34522b66658e412016bf0
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-03 17:40:37 +0700

    Implemented cancel context signal to stop gro routine and prevent go routine leaks

--- context_test.go
+++ context_test.go
@@ -3,7 +3,9 @@ package gocontext
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -36,4 +38,42 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextF.Value("f"))
 	fmt.Println(contextF.Value("c"))
 	fmt.Println(contextF.Value("b"))
+}
+func CreateCounter(ctx context.Context) chan int {
+
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Go-routine", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	detstination := CreateCounter(ctx)
+	for n := range detstination {
+		fmt.Println("Counter:", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+	time.Sleep(2 * time.Second)
+	fmt.Println("Total goroutine", runtime.NumGoroutine())
+
 }
